5c6ca4df6387c51acd680310afd77d24edd78cef
leeny_the_bear <leeny@inbox.ru>
2022-11-28 16:07:22 +0600

    add bot initialization to the class

--- tg_bot.py
+++ tg_bot.py
@@ -12,14 +12,15 @@ logger = logging.getLogger('support_bot')
 
 
 class BotLogsHandler(logging.Handler):
-    def __init__(self, telegram_bot, telegram_chat_id):
+    def __init__(self, telegram_token, telegram_chat_id):
         super().__init__()
-        self.telegram_bot = telegram_bot
-        self.telegram_chat_id = telegram_chat_id
+        self.token = telegram_token
+        self.chat_id = telegram_chat_id
+        self.bot = Bot(token=self.token)
 
     def emit(self, record: logging.LogRecord) -> None:
         log_entry = self.format(record)
-        self.telegram_bot.send_message(chat_id=self.telegram_chat_id, text=log_entry)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update: Update, context: CallbackContext) -> None:
