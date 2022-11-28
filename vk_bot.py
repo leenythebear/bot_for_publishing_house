87cdcef1ef2d2a32f94b15e9693030818c740903
leeny_the_bear <leeny@inbox.ru>
2022-11-28 11:03:09 +0600

    add logger to vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -3,9 +3,13 @@ import random
 from dotenv import load_dotenv
 
 import vk_api as vk
+from vk_api import ApiError
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from google_api import detect_intent_texts
+
+
+logger = logging.getLogger('support_bot')
 
 
 def echo(user_id, text, vk_api):
@@ -17,8 +21,15 @@ def echo(user_id, text, vk_api):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.WARNING
+    )
+
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
+    project_id = os.environ['PROJECT_ID']
+
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
@@ -26,11 +37,17 @@ if __name__ == "__main__":
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             message = event.message
             user_id = event.user_id
-            is_fallback, msg_text = detect_intent_texts(project_id=project_id,
+            try:
+                is_fallback, msg_text = detect_intent_texts(project_id=project_id,
                                            session_id=user_id,
                                            text=message,
                                            language_code='RU')
-            if not is_fallback:
-                echo(user_id, msg_text, vk_api)
+                if not is_fallback:
+                    echo(user_id, msg_text, vk_api)
+            except ApiError as error:
+                error_code = error.error.get('error_code')
+                error_text = error.error.get('error_msg')
+                logger.error(f'{error_code} {error_text}')
+
 
 
